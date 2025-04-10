0eaefe38774673377bea757137d1a9d1d0845d38
Jan Metzler <metzler.infa@gmail.com>
2025-04-10 02:47:59 +0200

    Create slurm-epilog.sh

---
+++ system-config/slurm-epilog.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+   H=$(hostname)
+   if [ "${H%-*}" != "dcc" ]
+   then
+       exit 0
+   fi
+   if [ -n "$SLURM_JOB_USER" -a "$SLURM_JOB_USER" != "root" ]
+   then
+       pkill -u $SLURM_JOB_USER
+       sleep 2
+       pkill -9 -u $SLURM_JOB_USER
+   fi
+   exit 0
