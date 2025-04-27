fce91ec7e9b4c30ef647b039288a7a0f24a1f248
Jan Metzler <metzler.infa@gmail.com>
2025-04-27 22:22:26 +0200

    Update check_machine_availability.sh

--- system-config/check_machine_availability.sh
+++ system-config/check_machine_availability.sh
@@ -4,16 +4,18 @@
  minVal=$(echo $machines | sed -nE 's/\S+\[([0-9]+)-[0-9]+\]/\1/p') #get the lowest index
  maxVal=$(echo $machines | sed -nE 's/\S+\[[0-9]+-([0-9]+)\]/\1/p') #get the highest index
 
- echo "checking state of machines ${machines}"
+ echo "Checking state of machines ${machines}..."
  
  for i in $(seq $minVal $maxVal); #for every machine in the cluster
  do
    machine=$name$i
+   echo "Pinging ${machine}..."
    if ping -c1 $machine > /dev/null 2>&1; #ping once and redirect stdout and stderr to /dev/null
    then #if ping was successful
      echo "${machine} alive";
    else #if machine could not be pinged
+     echo "Unable to contact ${machine}"
      wol $(arp -a $machine | grep -oP '(([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}))') > /dev/null #get the mac address of the machine from the arp table then wake on lan
-     echo "waking up ${machine}..."
+     echo "Waking up ${machine}..."
    fi;
  done;
