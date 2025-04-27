582cd7097e435dd4be0f2a76d1fb7fe0133df6d7
Jan Metzler <metzler.infa@gmail.com>
2025-04-27 02:24:12 +0200

    Update check_machine_config.sh

--- system-config/check_machine_config.sh
+++ system-config/check_machine_config.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+systemctl status slurmd
+systemctl status munge
+
+getent passwd voytek $ ldapsearch -LLL -ZZ -x '(uid=voytek)' dn
+munge -n | unmunge 
+
 if test -f /etc/munge/munge.key && sha512sum /etc/munge/munge.key | grep 'cde7fc8b9b582f527d71f2e93c1b7441d08ed04219d6f342e351bd328a301bce0434e2a2b9eb1d4fb68c7bbd1cb48d1518cafb5f71c08a3071ec4d20e50bff83' -q;
 then
 echo "found munge key";
