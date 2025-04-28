99a5f06008f5ab1fde0c2ccbc4c99ee383546596
Jan Metzler <metzler.infa@gmail.com>
2025-04-28 12:39:52 +0200

    Update check_machine_config.sh

--- system-config/check_machine_config.sh
+++ system-config/check_machine_config.sh
@@ -31,12 +31,14 @@ echo "did not find munge key";
 reboot
 fi;
 
+#wickd - daemon do sieci; ifstatus enp1s0
 if test -f /sys/class/net/enp1s0/operstate && cat /sys/class/net/enp1s0/operstate | grep 'up' -q; 
 then
 echo "enp1s0 up";
 else
 echo "enp1s0 down";
 ip link set dev enp1s0 up
+#zrestartowac port na switchu
 fi;
 
 if snmpget -v2c -cpublic 192.168.0.239 ifOperStatus.39 | grep 'down' -q;
@@ -45,3 +47,8 @@ echo "Port on the switch is down..."
 snmpset -v3 -uadmin -aSHA -Aswitch10G -xDES -Xswitch10G -l authPriv 192.168.0.239 ifAdminStatus.39 i 1
 echo "Setting up port on the switch..."
 fi;
+
+#lab-sec ma porty 1-16
+#lab-net ma porty 17-32
+#snmpbulkwalk -v2c -c public 192.168.0.239 iso.3.6.1.2.1.17.7.1.2.2.1.2.1 | perl -pe ' s/SNMPv2-SMI::mib-2.17.7.1.2.2.1.2.1.//; while (/(\d+)/mg) { printf("%02x:", $1) } printf "  " '
+#scontrol show node lab-net-57
