16f34ca2d21f6ff5b4dae2109312080cf85ba67a
Jan Metzler <metzler.infa@gmail.com>
2025-05-18 20:45:16 +0200

    Update and rename check_machine_config.sh to check_connectivity.sh

--- system-config/check_connectivity.sh
+++ system-config/check_connectivity.sh
@@ -1,36 +1,4 @@
 #!/bin/bash
-
-if systemctl status slurmd | grep 'active (running)' -q;
-then
-echo "slurmd up"
-else
-echo "slurmd down"
-systemctl start slurmd
-fi;
-
-if systemctl status munge | grep 'active (running)' -q && munge -n | grep -q 'MUNGE:';
-then
-echo "munge up"
-else
-echo "munge down"
-systemctl start munge
-fi;
-
-if getent passwd voytek | grep -q '' && ldapsearch -LLL -ZZ -x '(uid=voytek)' dn | grep -q '';
-then 
-echo "ldap  up"
-else 
-echo "ldap down"
-fi;
-
-if test -f /etc/munge/munge.key && sha512sum /etc/munge/munge.key | grep 'cde7fc8b9b582f527d71f2e93c1b7441d08ed04219d6f342e351bd328a301bce0434e2a2b9eb1d4fb68c7bbd1cb48d1518cafb5f71c08a3071ec4d20e50bff83' -q;
-then
-echo "found munge key";
-else 
-echo "did not find munge key";
-reboot
-fi;
-
 #wickd - daemon do sieci; ifstatus enp1s0
 if test -f /sys/class/net/enp1s0/operstate && cat /sys/class/net/enp1s0/operstate | grep 'up' -q; 
 then
