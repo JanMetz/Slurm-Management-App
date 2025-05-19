995e46569fffcc921f52debefed3781674a1e8c5
Jan Metzler <metzler.infa@gmail.com>
2025-05-19 17:18:34 +0200

    Update slurm-epilog.sh

--- system-config/slurm-epilog.sh
+++ system-config/slurm-epilog.sh
@@ -10,5 +10,6 @@
        pkill -u $SLURM_JOB_USER
        sleep 2
        pkill -9 -u $SLURM_JOB_USER
+       who | awk '$1 == "$SLURM_JOB_USER" { print $2 }' | xargs -r -n1 pkill -t
    fi
    exit 0
