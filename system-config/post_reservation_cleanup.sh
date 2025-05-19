d611aed46cda77db7cd932a63cd0e99abbe96f9e
Jan Metzler <metzler.infa@gmail.com>
2025-05-19 15:25:27 +0200

    Update post_reservation_cleanup.sh

--- system-config/post_reservation_cleanup.sh
+++ system-config/post_reservation_cleanup.sh
@@ -3,5 +3,3 @@
 machines=$(sed -nE 's/^PartitionName=\S+\s+Nodes=(\S+)\s+Default=\S+\s?$/\1/p' /etc/slurm/slurm.conf) #get all machines from the cluster
 
 scontrol update NodeName=$machines State=RESUME # reset to IDLE
-
-echo "Cleanup done at $(date)" >> /var/log/slurm/cleanup.log
