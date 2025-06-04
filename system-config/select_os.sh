c46a2170f8956199311772712f376d77c5cdf58e
Jan Metzler <metzler.infa@gmail.com>
2025-06-04 23:56:10 +0200

    Update select_os.sh

--- system-config/select_os.sh
+++ system-config/select_os.sh
@@ -4,8 +4,8 @@ machines=$(sed -nE 's/^PartitionName=\S+\s+Nodes=(\S+)\s+Default=\S+\s?$/\1/p' /
 name=$(echo $machines | sed -nE 's/(\S+)\[[0-9]+-[0-9]+\]/\1/p') #get the machine name prefix
 minVal=$(echo $machines | sed -nE 's/\S+\[([0-9]+)-[0-9]+\]/\1/p') #get the lowest index
 maxVal=$(echo $machines | sed -nE 's/\S+\[[0-9]+-([0-9]+)\]/\1/p') #get the highest index
- 
-LAUNCH_CORRECT_OS="grub2-once $(grub2-once --list | sed -nE "s/^\s+([0-9]+)\s+VLab$/\1/p"); reboot" # ZMIENIC VLAB NA NAZWE OS DO OBLICZEN!!!
+
+LAUNCH_CORRECT_OS="if ! test -f "etc/slurm/i_am_node"; then grub2-once $(grub2-once --list | sed -nE "s/^\s+([0-9]+)\s+openSUSE Leap 15.6 Calc$/\1/p"); reboot; fi" # ZMIENIC VLAB NA NAZWE OS DO OBLICZEN!!!
  
  for i in $(seq $minVal $maxVal); #for every machine in the cluster
  do
