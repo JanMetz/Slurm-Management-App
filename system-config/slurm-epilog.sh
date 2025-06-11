753eeb089e65c7a16e3ba4c4b6ef3264acefa090
Jan Metzler <metzler.infa@gmail.com>
2025-06-11 02:21:43 +0200

    Update slurm-epilog.sh

--- system-config/slurm-epilog.sh
+++ system-config/slurm-epilog.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+#!/bin/bash
+
 if [ -n "$SLURM_JOB_USER" -a "$SLURM_JOB_USER" != "root" ]
 then
  HOSTNAME=$(hostname)
- if [ ! (squeue -o "%u %N" | grep "$SLURM_JOB_USER $HOSTNAME" -q) ] then
-    who | awk '$1 == "$SLURM_JOB_USER" { print $2 }' | xargs -r -n1 pkill -t
+ if ! squeue -o "%u %N" | grep -q "$SLURM_JOB_USER $HOSTNAME"; then
+    who | awk -v user="$SLURM_JOB_USER" '$1 == user { print $2 }' | \
+        xargs -r -n1 pkill -t
  fi
 fi
 exit 0
