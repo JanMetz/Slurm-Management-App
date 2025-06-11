2b0f1fd5970bf1d8dd8d915319856cfef6acbd66
Jan Metzler <metzler.infa@gmail.com>
2025-06-11 02:32:07 +0200

    Update slurm-resume.sh

--- system-config/slurm-resume.sh
+++ system-config/slurm-resume.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+restart_with_correct_os(){ #$1=hostname maszyny
+     ssh root@$1 "grub2-once 3; reboot" #musza koniecznie byc klucze!
+}
+
 machines=$(sed -nE 's/^PartitionName=\S+\s+Nodes=(\S+)\s+Default=\S+\s?$/\1/p' /etc/slurm/slurm.conf) #get all machines from the cluster
 name=$(echo $machines | sed -nE 's/(\S+)\[[0-9]+-[0-9]+\]/\1/p') #get the machine name prefix
 minVal=$(echo $machines | sed -nE 's/\S+\[([0-9]+)-[0-9]+\]/\1/p') #get the lowest index
@@ -15,6 +19,9 @@ do
   if ping -c1 $machine > /dev/null 2>&1; #ping once and redirect stdout and stderr to /dev/null
   then #if ping was successful
     echo "${machine} alive";
+    #intel amt restart
+    sleep 180s;
+    restart_with_correct_os($machine)
   else #if machine could not be pinged
     echo "Unable to contact ${machine}"
     mac=$(cat /etc/ethers | sed -nE "s/(^[^#]+)\s+$machine/\1/p"); #get the mac address of the machine from the ethers table
@@ -22,6 +29,7 @@ do
     then
       echo "Waking up ${machine}...";
       wol $mac > /dev/null; # wake on lan
+      restart_with_correct_os($machine)
     else
       echo "No entry in the ethers table for ${machine}";
     fi;
