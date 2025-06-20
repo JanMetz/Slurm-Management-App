cbe2760e445105b2c45aa741d796e020c1213fc0
Jan Metzler <metzler.infa@gmail.com>
2025-06-20 02:45:46 +0200

    Update slurm-suspend.sh

--- system-config/slurm-suspend.sh
+++ system-config/slurm-suspend.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
- logger -t SLURM "suspending $@"
- for HOST in $(scontrol show hostnames "$1")
- do
-   HNO=${HOST##*-}
-   ipmitool -f /etc/ipmi.secret -I lan -H 10.10.0.$((150+$HNO)) -U root \
-            power soft
- done
+for node in $@
+do
+  #amttool softpoweroff
+done
 
- exit 0
+exit 0
