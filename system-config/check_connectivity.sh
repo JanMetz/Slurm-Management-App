a4496f7e9722cfc61b8bd632625aa87d703218da
Jan Metzler <metzler.infa@gmail.com>
2025-06-25 17:43:08 +0200

    Update check_connectivity.sh

--- system-config/check_connectivity.sh
+++ system-config/check_connectivity.sh
@@ -11,6 +11,11 @@ snmpset -v3 -uadmin -aSHA -Aswitch10G -xDES -Xswitch10G -l authPriv 192.168.0.23
 ip link set dev enp1s0 up
 fi;
 
+#ifspeed 
+#jeżeli ifspeed zawiedzie to jeszcze jeden restart
+#ifspeed
+#jeżeli ifspeed znowu zawiedzie to ustawić stan węzła na failed
+
 #lab-sec ma porty 1-16
 #lab-net ma porty 17-32
 #snmpbulkwalk -v2c -c public 192.168.0.239 iso.3.6.1.2.1.17.7.1.2.2.1.2.1 | perl -pe ' s/SNMPv2-SMI::mib-2.17.7.1.2.2.1.2.1.//; while (/(\d+)/mg) { printf("%02x:", $1) } printf "  " '
