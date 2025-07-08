f72609ce641939ca36fe24dbbbf5e06b57447a80
Jan Metzler <metzler.infa@gmail.com>
2025-07-08 22:01:49 +0200

    Update run_amt.sh

--- tmp/run_amt.sh
+++ tmp/run_amt.sh
@@ -1,4 +1,6 @@
  #!/bin/bash
+HOST=$1;
+PORT=$(cat /etc/amt/passwords | sed -nE "s/(^[^#]+)\s+$HOST/\1/p"); #zakladajac format pliku passwords analogiczny do formatu pliku ethers tj haslo, biale znaki, hostname
 export AMT_PASSWORD="secret_password"
 amttool $@
 export AMT_PASSWORD=""
