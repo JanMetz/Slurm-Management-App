4f2328c3bdc360bb1104f97532135c937a78f3fe
Jan Metzler <metzler.infa@gmail.com>
2025-08-31 23:55:41 +0200

    Update rollback_setup.sh

--- system-config/rollback_setup.sh
+++ system-config/rollback_setup.sh
@@ -1,35 +1,47 @@
 #!/bin/bash
 
+restore_config_files(){ #$1=path-to-file 
+  if test -f $1; 
+  then
+    mv $1  .
+    
+    if test -f $1.old;
+    then
+      mv $1.old $1;
+    fi;
+  fi;
+}
+
 echo +++ [INFO] Uruchomiono skrypt rollback...
-mv /etc/nsswitch.conf.old        /etc/nsswitch.conf
-mv /etc/ldap.conf.old            /etc/ldap.conf
-mv /etc/openldap/ldap.conf.old   /etc/openldap/ldap.conf
-mv /etc/auto.master.old          /etc/auto.master
-mv /etc/auto.home.old            /etc/auto.home
-mv /etc/auto.a.old               /etc/auto.a
-mv /etc/sysconfig/autofs.old     /etc/sysconfig/autofs
+restore_config_files  /etc/nsswitch.conf            
+restore_config_files  /etc/ldap.conf
+restore_config_files  /etc/openldap/ldap.conf       
+restore_config_files  /etc/auto.master
+restore_config_files  /etc/auto.home
+restore_config_files  /etc/auto.a
+restore_config_files  /etc/sysconfig/autofs
 
-mv /etc/pam.d/sshd.old                /etc/pam.d/sshd
-mv /etc/pam.d/common-account-pc.old   /etc/pam.d/common-account-pc
-mv /etc/pam.d/common-auth-pc.old      /etc/pam.d/common-auth-pc
-mv /etc/pam.d/common-session-pc.old   /etc/pam.d/common-session-pc
-mv /etc/pam.d/common-password-pc.old  /etc/pam.d/common-password-pc
+restore_config_files  /etc/pam.d/sshd
+restore_config_files  /etc/pam.d/common-account-pc
+restore_config_files  /etc/pam.d/common-auth-pc
+restore_config_files  /etc/pam.d/common-session-pc
+restore_config_files  /etc/pam.d/common-password-pc
 
-mv /etc/security/access.conf.old     /etc/security/access.conf
-mv /etc/ssh/sshd_config.old          /etc/ssh/sshd_config
+restore_config_files  /etc/security/access.conf
+restore_config_files  /etc/ssh/sshd_config
 
-mv /etc/my.cnf.old  /etc/my.cnf
+restore_config_files  /etc/my.cnf
 
-mv /etc/slurm/slurm.conf.old        /etc/slurm/slurm.conf
-mv /etc/slurm/slurm-epilog.sh.old   /etc/slurm/slurm-epilog.sh
-mv /etc/slurm/slurm-resume.sh.old   /etc/slurm/slurm-resume.sh
-mv /etc/slurm/slurm-suspend.sh.old  /etc/slurm/slurm-suspend.sh
-mv /etc/slurm/slurmdbd.conf.old     /etc/slurm/slurmdbd.conf
-mv /etc/slurm/acct_gather.conf.old  /etc/slurm/acct_gather.conf
+restore_config_files  /etc/slurm/slurm.conf
+restore_config_files  /etc/slurm/slurm-epilog.sh
+restore_config_files  /etc/slurm/slurm-resume.sh
+restore_config_files  /etc/slurm/slurm-suspend.sh
+restore_config_files  /etc/slurm/slurmdbd.conf
+restore_config_files  /etc/slurm/acct_gather.conf
 
-mv /etc/ethers.old                  /etc/ethers
+restore_config_files  /etc/ethers
 
-mv /etc/systemd/system/slurmd.service.d/override.conf.old     /etc/systemd/system/slurmd.service.d/override.conf
-mv /etc/systemd/system/slurmctld.service.d/override.conf.old  /etc/systemd/system/slurmctld.service.d/override.conf
+restore_config_files  /etc/systemd/system/slurmd.service.d/override.conf
+restore_config_files  /etc/systemd/system/slurmctld.service.d/override.conf
 
 echo +++ [INFO] Wykonano skrypt rollback!
