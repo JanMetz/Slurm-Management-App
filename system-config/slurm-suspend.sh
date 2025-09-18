3ea317bd3b51049be28f67bb6caa0e02312f978c
Jan Metzler <metzler.infa@gmail.com>
2025-09-18 22:21:26 +0200

    Update slurm-suspend.sh

--- system-config/slurm-suspend.sh
+++ system-config/slurm-suspend.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-for node in $@
+echo "Resuming $@"
+URL='wss://mesh.cs.put.poznan.pl';
+USR='XXX';
+PWD='YYY';
+
+run_meshctrl_command(){ #$1=device power command
+        DEV_ID=$(node /etc/slurm/meshctrl.js ListDevices --url $URL --loginuser $USR --loginpass $PWD --json | jq -r --arg host "$node" '.[] | select((.name | ascii_downcase)==$host) | ._id')
+        node /etc/slurm/meshctrl.js DevicePower --$1 --url $URL --loginuser $USR --loginpass $PWD --id $DEV_ID
+        sleep 15;
+}
+
+for node in $@;
 do
-  #amttool softpoweroff
-done
+    echo "Initiating power saving mode...";
+    run_meshctrl_command off;
+    sleep 10;
+    run_meshctrl_command amtoff;
+done;
 
 exit 0
