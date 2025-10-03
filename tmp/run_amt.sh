03f283a1b6ee8901ab47a1982cc81ae343724a77
Jan Metzler <metzler.infa@gmail.com>
2025-10-03 17:08:33 +0200

    Delete tmp directory

--- tmp/run_amt.sh
+++
@@ -1,6 +0,0 @@
- #!/bin/bash
-HOST=$1;
-PWD=$(cat /etc/amt/passwords | sed -nE "s/(^[^#]+)\s+$HOST/\1/p"); #zakladajac format pliku passwords analogiczny do formatu pliku ethers tj haslo, biale znaki, hostname
-export AMT_PASSWORD=$PWD
-amttool $@
-export AMT_PASSWORD=""
